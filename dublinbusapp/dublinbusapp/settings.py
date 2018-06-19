9b6a57f7c8ee554449366535eed60d0234315b59
Dillon Friel <dillonfriel@gmail.com>
2018-06-19 12:23:48 +0100

    connected to sample database

--- dublinbusapp/dublinbusapp/settings.py
+++ dublinbusapp/dublinbusapp/settings.py
@@ -77,7 +77,11 @@ WSGI_APPLICATION = 'dublinbusapp.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': 'dublinbusdb',
+	'USERNAME': 'dublinbususer',
+	'PASSWORD': 'dublinbuspass',
+	'HOST': 'dublinbusdb.cww5dmspazsv.ue-west-1.rds.amazonaws.com',
+	'PORT': '3306',
     }
 }
 
