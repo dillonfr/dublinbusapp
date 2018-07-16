476ff6817ab857043e7f138f7ece9d7da30c8b8d
dillonfr <dillonfriel@gmail.com>
2018-07-16 10:11:08 +0100

    forms added

--- dublinbusapp/dublinbusapp/views.py
+++ dublinbusapp/dublinbusapp/views.py
@@ -1,29 +1,30 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-#from .models import Bus
 from .models import Trips2017
 from dict import *
 from .forms import *
 
 def index(request):
 	buslist = makeBusStopDict()
+
+	#context is a dictionary that will contain anything we want to send to the frontend
 	context = {
 		'buslist': buslist,
 	}
 
-
+	
+	#if user clicks the find route button on the frontend, this sends a post request to django
 	if request.method == 'POST':
-		print("method is post")
 		form = StartForm(request.POST)
 		
 		#start_id = form.cleaned_data['start_id']
 		
 		#context['start_id'] = start_id
-	
+		
+		#checks the info input into the form to make sure it is legal (security check, checks theres no SQL injection etc.)
 		if form.is_valid():
 			start_id = form.cleaned_data['start_id']
 			#end_id = form.cleaned_data['end_id']
-			print("OK")
 			print("Start: ", start_id)
 			#print("End: ", end_id)	
 			context['start_id'] = start_id
@@ -33,6 +34,7 @@ def index(request):
 	else:
 		form = StartForm()
 	
+	#how to query database: (add query_results to context to send to frontend)
 	#query_results = Trips2017.objects.raw('SELECT * FROM trips2017 LIMIT 20')
 
 	#context = {
