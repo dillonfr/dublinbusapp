acf0384c406daa13bf17f609856f74cd5a85f1d1
dillonfr <dillonfriel@gmail.com>
2018-07-27 11:17:46 +0100

    Revert "ready to merge"

--- dublinbusapp/dublinbusapp/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-#from .models import Bus
-
-#register the Bus class as an admin site
-#admin.site.register(Bus)
--- dublinbusapp/dublinbusapp/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-
-class StartForm(forms.Form):
-	start_id = forms.CharField(label="Start ID", max_length=50)
-	end_id = forms.CharField(label="End ID", max_length=50)	
-
-class NameForm(forms.Form):
-	your_name = forms.CharField(label='Your name', max_length=100)
